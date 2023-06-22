d94bc584f7d00f329bbc929fd4ba1b9e3920e6ed
J. Alejandro Coronado Cuesta <tk.jalejandro@gmail.com>
2023-06-22 11:28:52 +0200

    Styles were scoped for each project app and ma

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,16 +3,23 @@ import { mount } from 'marketing/MarketingApp'
 import MarketingApp from './components/MarketingApp'
 import Header from './components/Header'
 import { BrowserRouter } from 'react-router-dom'
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
 
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'app'
+})
 console.log(mount)
 const App = () => {
 
-
     return (
+
         <BrowserRouter>
-           <Header />
-            <MarketingApp />
+            <StylesProvider generateClassName={generateClassName}>
+                <Header />
+                <MarketingApp />
+            </StylesProvider>
         </BrowserRouter>
+
     )
 }
 

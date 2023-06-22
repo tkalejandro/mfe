32f52c1cf04dd7ecdf32526c643a2f2a92018e55
J. Alejandro Coronado Cuesta <tk.jalejandro@gmail.com>
2023-06-22 10:52:23 +0200

    header was added.

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import { mount } from 'marketing/MarketingApp'
 import MarketingApp from './components/MarketingApp'
+import Header from './components/Header'
+import { BrowserRouter } from 'react-router-dom'
 
 console.log(mount)
 const App = () => {
 
 
     return (
-        <>
-            <div>My App Container</div>
-            <hr />  
+        <BrowserRouter>
+           <Header />
             <MarketingApp />
-        </>
+        </BrowserRouter>
     )
 }
 
